715eec030d2bd66e3d71de3181c7129ae68d5585
clickclickcode <thanatosanyone@gmail.com>
2023-11-26 14:45:08 +0100

    adding to the 'new' page

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,5 +1,27 @@
+import Link from 'next/link'
+
+// Here we are creating a server action that will create a new todo item in our database
+
+
 export default function New() {
     return (
-        <h1>Hello from the new side</h1>
+        <>
+            <header className='flex justify-between items-center mb-4'>
+                <h1 className="text-2xl">New</h1>
+            </header>
+            <form className="flex gap-2 flex-col">
+                <input
+                    type="text"
+                    name="title"
+                    className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"
+                />
+                <div className='flex gap-1 justify-end'>
+                    {/* typing href='..' is essentially the same as when navigating folders in the terminal */}
+                    {/* typing href='..' is equivalent to 'cd ..' so clicking the 'Cancel' Link/button will send us back to the root */}
+                    <Link href='..' className='border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none'>Cancel</Link>
+                    <button type='submit' className='border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none'>Create</button>
+                </div>
+            </form>
+        </>
     )
 }
