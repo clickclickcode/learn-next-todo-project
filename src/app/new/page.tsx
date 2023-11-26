5cde21fc6692f30dc94c735fce13c0c465f7778e
clickclickcode <thanatosanyone@gmail.com>
2023-11-26 13:58:22 +0100

    adding new route and Link tags

---
+++ src/app/new/page.tsx
@@ -0,0 +1,5 @@
+export default function New() {
+    return (
+        <h1>Hello from the new side</h1>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <h1>
-      Hello
-    </h1>
+    <>
+      <header>
+        <h1 className="text-2xl">Todos</h1>
+        {/* These Link tags work just like an anchor tag, but needs to be imported prior to use */}
+        <Link href='/new'>New</Link>
+      </header>
+      <ul></ul>
+    </>
   )
 }
